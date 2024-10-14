0540089a5a791b8583f47e95e43efa3f496c136f
charan.panamata <charan.panamata@192.168.5.79>
2024-10-14 12:47:28 +0530

    dyrjdyr

---
+++ eclipse-workspace/10oct2024/src/test/java/packexample/exe.java
@@ -0,0 +1,16 @@
+package packexample;
+import oct_pack.*;
+
+public class exe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("guys");
+		oct_pack.objects.main(args); 
+		oct_pack.Variable.main(args);
+		
+
+	}
+
+}
